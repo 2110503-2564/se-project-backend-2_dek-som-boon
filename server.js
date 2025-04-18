3002d0315696c46c21c80318b9c3f98ba36a55ba
nanthapak <nanthapak109@gmail.com>
2025-04-19 02:43:17 +0700

    cors problem

--- server.js
+++ server.js
@@ -2,62 +2,62 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db');
-const mongoSanitize=require('express-mongo-sanitize');
-const helmet=require('helmet');
-const {xss}=require('express-xss-sanitizer');
-const rateLimit=require('express-rate-limit');
-const hpp=require('hpp');
-const cors=require('cors');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const { xss } = require('express-xss-sanitizer');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 
-//Load env vars
-dotenv.config({path:'./config/config.env'});
+// Load env vars
+dotenv.config({ path: './config/config.env' });
 
 connectDB();
 
 const app = express();
 
-// Route files
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://antony-massage.vercel.app/'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
+
+app.use(express.json());
+app.use(mongoSanitize());
+app.use(helmet());
+app.use(xss());
+
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 100
+});
+app.use(limiter);
+app.use(hpp());
+app.use(cookieParser());
+
 const massageShops = require('./routes/massage-shops');
 const auth = require('./routes/auth');
 const reservations = require('./routes/reservations');
 const users = require('./routes/users');
 const reviews = require('./routes/reviews');
-// Body parser
-app.use(express.json());
-
-//Sanitize data
-app.use(mongoSanitize());
-//Set security headers
-app.use(helmet());
-//Prevent XSS attacks
-app.use(xss());
-//Rate Limiting
-const limiter=rateLimit({
-    windowsMs:10*60*1000,//10 mins
-    max: 100
-});
-app.use(limiter);
-//Prevent http param pollutions
-app.use(hpp());
-//Enable CORS
-app.use(cors());
-
-// Cookie parser
-app.use(cookieParser());
 
 app.use('/api/v1/massage-shops', massageShops);
 app.use('/api/v1/auth', auth);
 app.use('/api/v1/reservations', reservations);
 app.use('/api/v1/users', users);
 app.use('/api/v1/reviews', reviews);
+
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, console.log('Server running in', process.env.NODE_ENV, 'mode on port', PORT));
+const server = app.listen(PORT, () =>
+  console.log('Server running in', process.env.NODE_ENV, 'mode on port', PORT)
+);
 
-
-// Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-    console.log(`Error: ${err.message}`);
-    // Close server & exit process
-    server.close(() => process.exit(1));
-})
+  console.log(`Error: ${err.message}`);
+  server.close(() => process.exit(1));
+});
